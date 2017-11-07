3b302ae6558a5c904b4d21bb463ab2d63ea6d75b
Lucas <ii.fredin@gmail.com>
2017-11-07 13:17:56 +0100

    name change
    ksjad

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello , i´m jacob");
+		System.out.println("hello , i´m lucas");
 	}
 
 }
