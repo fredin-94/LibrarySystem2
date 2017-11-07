5e99d1defc5bf1a972fa0519b3af652545c73dfe
Trooper98 <gusmanzol@student.gu.se>
2017-11-07 17:46:33 +0100

    Menu created
    I've created a menu class that prints out different menus.

---
+++ src/library/Menu.java
@@ -0,0 +1,68 @@
+package library;
+
+public class Menu {
+
+	public void getMenu() {
+		System.out.println();
+		System.out.println(" =================================================== ");
+		System.out.println(" ==             Library System                    == ");
+		System.out.println(" =================================================== ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" ===   Choose an option below:                   === ");
+		System.out.println(" ===   1. Search library directory               === ");
+		System.out.println(" ===   2. Administartion                         === ");
+		System.out.println(" ===   4. Time Simulator FX40000                 === ");
+		System.out.println(" ===   5.Quit program                            === ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" =================================================== ");
+		System.out.println();
+	}
+	
+	public void getSearch() {
+		System.out.println();
+		System.out.println(" =================================================== ");
+		System.out.println(" ==             Search System                     == ");
+		System.out.println(" =================================================== ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" ===   Choose an option below:                   === ");
+		System.out.println(" ===   1. Search for book                        === ");
+		System.out.println(" ===   2. Search for user                        === ");
+		System.out.println(" ===   4.Quit search                             === ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" =================================================== ");
+		System.out.println();
+	}
+	
+	public void getAdministration() {
+		System.out.println();
+		System.out.println(" =================================================== ");
+		System.out.println(" ==             Administration System             == ");
+		System.out.println(" =================================================== ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" ===   Choose an option below:                   === ");
+		System.out.println(" ===   1. Add Book                               === ");
+		System.out.println(" ===   2. Remove Book                            === ");
+		System.out.println(" ===   4. Add User                               === ");
+		System.out.println(" ===   4. Remove User                            === ");
+		System.out.println(" ===   5.Quit program                            === ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" =================================================== ");
+		System.out.println();
+	}
+	
+	public void getSimulator() {
+		System.out.println();
+		System.out.println(" =================================================== ");
+		System.out.println(" ==             Simulator System                  == ");
+		System.out.println(" =================================================== ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" ===   Choose an option below:                   === ");
+		System.out.println(" ===   1. Increment Days                         === ");
+		System.out.println(" ===   2. Increment Months                       === ");
+		System.out.println(" ===   4. Increment Years                        === ");
+		System.out.println(" ===   5.Quit program                            === ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" =================================================== ");
+		System.out.println();
+	}
+}
