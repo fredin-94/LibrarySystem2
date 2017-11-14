b34ab8454443a80bc7da7fb0852d032030f09d1b
Lucas <ii.fredin@gmail.com>
2017-11-14 17:11:02 +0100

    Adding methods in Test, fixing up Menu
    been working on methods etc in Test,
    been changing things up in Menu class
    Added a class with commented code (just in case u want whats in it, can be deleted afterwards)
    added res folder, with 2 txt documents

---
+++ src/library/ImplementableCode.java
@@ -0,0 +1,51 @@
+package library;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ImplementableCode {
+
+	//HELLO! Here is some code you might want to implement. You might have to change the code a little..
+	//After these have been implemented we can in main implement the method for saving books and customers 
+	//to the lists, so that they will be saved even if we end the program and start it again
+	
+	
+	//Reading a text tile into arraylist:
+	//Books (implement in library):
+//	public void bookDirectory() throws FileNotFoundException {
+//		Scanner input = new Scanner(new File("res/bookDirectory.txt"));
+//	    input.useDelimiter("-|\n");
+//		
+//		while(input.hasNext()) {
+//	       
+//	        String title = input.next();
+//			String author = input.next();
+//			String publisher = input.next();
+//			String genre = input.next();
+//			String shelf = input.next();
+//	        
+//	        Book book = new Book(title, author, publisher, genre, shelf);
+//	        books.add(book);
+//	    }
+//	}
+	
+	//Customers: (also implement in library)
+//	public void customerDirectory() throws Exception {
+//		Scanner input = new Scanner(new File("res/customer.txt"));
+//	    input.useDelimiter("/|\n");
+//		
+//		while(input.hasNext()) {
+//	       
+//	        String name = input.next();
+//			String address = input.next();
+//			String phoneNumber = input.next();
+//		//	String phoneNumberInt = input2.next();
+//			String psn = input.next();
+//	        
+//			Customer customer = new Customer(name, address, phoneNumber, psn);
+//	        customers.add(customer);
+//	    }
+//	}
+	
+}
--- src/library/Menu.java
+++ src/library/Menu.java
@@ -10,9 +10,10 @@ public class Menu {
 		System.out.println(" ===                                             === ");
 		System.out.println(" ===   Choose an option below:                   === ");
 		System.out.println(" ===   1. Search library directory               === ");
-		System.out.println(" ===   2. Administartion                         === ");
+		System.out.println(" ===   2. Borrow/Return books                    === ");
+		System.out.println(" ===   3. Administartion                         === ");
 		System.out.println(" ===   4. Time Simulator FX40000                 === ");
-		System.out.println(" ===   5.Quit program                            === ");
+		System.out.println(" ===   Press Q to quit                           === ");
 		System.out.println(" ===                                             === ");
 		System.out.println(" =================================================== ");
 		System.out.println();
@@ -26,8 +27,25 @@ public class Menu {
 		System.out.println(" ===                                             === ");
 		System.out.println(" ===   Choose an option below:                   === ");
 		System.out.println(" ===   1. Search for book                        === ");
-		System.out.println(" ===   2. Search for user                        === ");
-		System.out.println(" ===   4.Quit search                             === ");
+		System.out.println(" ===   2. Show and sort books                    === ");
+		System.out.println(" ===   3. Search for customers                   === ");
+		System.out.println(" ===   4. Show all customers                     === ");
+		System.out.println(" ===   0. Quit search                            === ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" =================================================== ");
+		System.out.println();
+	}
+	
+	public void getBookOptions() {
+		System.out.println();
+		System.out.println(" =================================================== ");
+		System.out.println(" ==             Borrow or return books            == ");
+		System.out.println(" =================================================== ");
+		System.out.println(" ===                                             === ");
+		System.out.println(" ===   Choose an option below:                   === ");
+		System.out.println(" ===   1. Borrow book                            === ");
+		System.out.println(" ===   2. Return book                            === ");
+		System.out.println(" ===   0. Quit search                            === ");
 		System.out.println(" ===                                             === ");
 		System.out.println(" =================================================== ");
 		System.out.println();
@@ -42,9 +60,9 @@ public class Menu {
 		System.out.println(" ===   Choose an option below:                   === ");
 		System.out.println(" ===   1. Add Book                               === ");
 		System.out.println(" ===   2. Remove Book                            === ");
-		System.out.println(" ===   4. Add User                               === ");
-		System.out.println(" ===   4. Remove User                            === ");
-		System.out.println(" ===   5.Quit program                            === ");
+		System.out.println(" ===   3. Add Customer                           === ");
+		System.out.println(" ===   4. Remove Customer                        === ");
+		System.out.println(" ===   Press Q to quit                           === ");
 		System.out.println(" ===                                             === ");
 		System.out.println(" =================================================== ");
 		System.out.println();
@@ -59,8 +77,8 @@ public class Menu {
 		System.out.println(" ===   Choose an option below:                   === ");
 		System.out.println(" ===   1. Increment Days                         === ");
 		System.out.println(" ===   2. Increment Months                       === ");
-		System.out.println(" ===   4. Increment Years                        === ");
-		System.out.println(" ===   5.Quit program                            === ");
+		System.out.println(" ===   3. Increment Years                        === ");
+		System.out.println(" ===   Press Q to quit                           === ");
 		System.out.println(" ===                                             === ");
 		System.out.println(" =================================================== ");
 		System.out.println();
