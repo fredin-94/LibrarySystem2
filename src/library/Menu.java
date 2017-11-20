c02a110efc89d959094f2a3fbc776511f12a8b52
fabianfroding <fabian.froding@gmail.com>
2017-11-20 19:08:24 +0100

    Menu
    Added line separator to the menu prints. lol.

--- src/library/Menu.java
+++ src/library/Menu.java
@@ -4,78 +4,78 @@ public class Menu {
 
 	public void getMenu() {
 		System.out.println(
-		"\n =================================================== "
-		+ " ==             Library System                    == "
-		+ " =================================================== "
-		+ " ===                                             === "
-		+ " ===   Choose an option below:                   === "
-		+ " ===   1. Search library directory               === "
-		+ " ===   2. Borrow/Return books                    === "
-		+ " ===   3. Administartion                         === "
-		+ " ===   4. Time Simulator FX40000                 === "
-		+ " ===   Press Q to quit                           === "
-		+ " ===                                             === "
+		"   =================================================== \n"
+		+ " ==             Library System                    == \n"
+		+ " =================================================== \n"
+		+ " ===                                             === \n"
+		+ " ===   Choose an option below:                   === \n"
+		+ " ===   1. Search library directory               === \n"
+		+ " ===   2. Borrow/Return books                    === \n"
+		+ " ===   3. Administartion                         === \n"
+		+ " ===   4. Time Simulator FX40000                 === \n"
+		+ " ===   Press Q to quit                           === \n"
+		+ " ===                                             === \n"
 		+ " =================================================== \n");
 	}
 	
 	public void getSearch() {
 		System.out.println(
-		"\n =================================================== "
-		+ " ==             Search System                     == "
-		+ " =================================================== "
-		+ " ===                                             === "
-		+ " ===   Choose an option below:                   === "
-		+ " ===   1. Search for book                        === "
-		+ " ===   2. Show and sort books                    === "
-		+ " ===   3. Search for customers                   === "
-		+ " ===   4. Show all customers                     === "
-		+ " ===   0. Quit search                            === "
-		+ " ===                                             === "
+		"   =================================================== \n"
+		+ " ==             Search System                     == \n"
+		+ " =================================================== \n"
+		+ " ===                                             === \n"
+		+ " ===   Choose an option below:                   === \n"
+		+ " ===   1. Search for book                        === \n"
+		+ " ===   2. Show and sort books                    === \n"
+		+ " ===   3. Search for customers                   === \n"
+		+ " ===   4. Show all customers                     === \n"
+		+ " ===   0. Quit search                            === \n"
+		+ " ===                                             === \n"
 		+ " =================================================== \n");
 	}
 	
 	public void getBookOptions() {
 		System.out.println(
-		"\n =================================================== "
-		+ " ==             Borrow or return books            == "
-		+ " =================================================== "
-		+ " ===                                             === "
-		+ " ===   Choose an option below:                   === "
-		+ " ===   1. Borrow book                            === "
-		+ " ===   2. Return book                            === "
-		+ " ===   0. Quit search                            === "
-		+ " ===                                             === "
+		"   =================================================== \n"
+		+ " ==             Borrow or return books            == \n"
+		+ " =================================================== \n"
+		+ " ===                                             === \n"
+		+ " ===   Choose an option below:                   === \n"
+		+ " ===   1. Borrow book                            === \n"
+		+ " ===   2. Return book                            === \n"
+		+ " ===   0. Quit search                            === \n"
+		+ " ===                                             === \n"
 		+ " =================================================== \n");
 	}
 	
 	public void getAdministration() {
 		System.out.println(
-		"\n =================================================== "
-		+ " ==             Administration System             == "
-		+ " =================================================== "
-		+ " ===                                             === "
-		+ " ===   Choose an option below:                   === "
-		+ " ===   1. Add Book                               === "
-		+ " ===   2. Remove Book                            === "
-		+ " ===   3. Add Customer                           === "
-		+ " ===   4. Remove Customer                        === "
-		+ " ===   Press Q to quit                           === "
-		+ " ===                                             === "
+		"   =================================================== \n"
+		+ " ==             Administration System             == \n"
+		+ " =================================================== \n"
+		+ " ===                                             === \n"
+		+ " ===   Choose an option below:                   === \n"
+		+ " ===   1. Add Book                               === \n"
+		+ " ===   2. Remove Book                            === \n"
+		+ " ===   3. Add Customer                           === \n"
+		+ " ===   4. Remove Customer                        === \n"
+		+ " ===   Press Q to quit                           === \n"
+		+ " ===                                             === \n"
 		+ " =================================================== \n");
 	}
 	
 	public void getSimulator() {
 		System.out.println(
-		"\n =================================================== "
-		+ " ==             Simulator System                  == "
-		+ " =================================================== "
-		+ " ===                                             === "
-		+ " ===   Choose an option below:                   === "
-		+ " ===   1. Increment Days                         === "
-		+ " ===   2. Increment Months                       === "
-		+ " ===   3. Increment Years                        === "
-		+ " ===   Press Q to quit                           === "
-		+ " ===                                             === "
+		"   =================================================== \n"
+		+ " ==             Simulator System                  == \n"
+		+ " =================================================== \n"
+		+ " ===                                             === \n"
+		+ " ===   Choose an option below:                   === \n"
+		+ " ===   1. Increment Days                         === \n"
+		+ " ===   2. Increment Months                       === \n"
+		+ " ===   3. Increment Years                        === \n"
+		+ " ===   Press Q to quit                           === \n"
+		+ " ===                                             === \n"
 		+ " =================================================== \n");
 	}
 }
