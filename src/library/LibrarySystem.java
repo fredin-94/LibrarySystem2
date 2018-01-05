d82c53b33ed8b822a1e907cc3904633064c3ce4c
Lucas <ii.fredin@gmail.com>
2018-01-05 11:55:29 +0100

    method author

--- src/library/LibrarySystem.java
+++ src/library/LibrarySystem.java
@@ -9,7 +9,7 @@ import library.Library.bookKey;
 import static library.Library.bookKey.*; // Needed to take enum keys as parameters. //Fabian.
 
 /**
- * <I, the author, am a jackass!>
+ * @author Hanien Kobus and Lucas Fredin
  * 
  * @author Oliver Manzi
  * @author Fabian Fröding
